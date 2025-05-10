0a65e910cc2fe29c6077c4321b77c6d41dc42dd0
daniel <danicvan@hotmail.com>
2025-05-09 22:46:59 -0300

    feat: integrate Resend API to send welcome emails on user registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UserModule } from './user/user.module';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [UserModule, NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
