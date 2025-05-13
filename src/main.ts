4228f676a14b4c9a27e8b36cd13d18b3abb9759f
daniel <danicvan@hotmail.com>
2025-05-12 23:21:48 -0300

    docs(swagger): customize UI with logo, description, contact and versioning

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,23 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('NotifyMe API')
-    .setDescription('API para envio de notificações')
+    .setDescription('Backend for managing user notifications and logs')
     .setVersion('1.0')
-    .addTag('notificações')
+    .addServer('/v1') // versionamento da API
+    .setContact('Daniel Camilo', 'https://github.com/danicvan', 'danicvan@hotmail.com')
+    .setExternalDoc('Documentação Oficial', 'https://notifyme.docs.com') // opcional
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('/v1/api', app, document, {
+    customSiteTitle: 'NotifyMe Docs',
+    customfavIcon: 'https://raw.githubusercontent.com/danicvan/assets/main/favicon.ico',
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customCss: `
+      .topbar-wrapper img {content:url('https://raw.githubusercontent.com/danicvan/assets/main/logo.png'); height:40px;}
+    `,
+  });
 
   await app.listen(3000);
 }
