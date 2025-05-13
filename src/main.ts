777b4a8d42c6447ca1a5bda9fe357a072b008dd2
daniel <danicvan@hotmail.com>
2025-05-13 00:01:05 -0300

    docs(swagger): enable swagger UI on /v1/api route

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configurar versão e rota global
   app.setGlobalPrefix('v1');
 
   const config = new DocumentBuilder()
@@ -13,13 +12,22 @@ async function bootstrap() {
     .setDescription('API for sending notifications and logging events.')
     .setVersion('1.0')
     .addTag('Users')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token', // esse nome será usado no Swagger para identificar o auth
+    )
     .setContact('Daniel Camilo', 'https://github.com/danicvan', 'danicvan@hotmail.com')
     .setExternalDoc('GitHub Repository', 'https://github.com/danicvan/notifyme-backend')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('v1/api', app, document); // 👉 Agora acessível em /v1/api
+  SwaggerModule.setup('v1/api', app, document);
 
   await app.listen(3000);
   console.log(`🚀 API running at http://localhost:3000/v1/api`);
